4a9fa773c06452831616b958976629425438227f
Toqeer-ux <toqeerbusinessinfo@gmail.com>
2025-01-16 17:03:20 +0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,143 @@
+// JavaScript Code
+document.addEventListener('DOMContentLoaded', () => {
+  const navLinks = document.querySelectorAll('#sidebar-nav .nav-link');
+  const activeLinkId = localStorage.getItem('activeLinkId');
+  if (activeLinkId) {
+      const activeLink = document.getElementById(activeLinkId);
+      if (activeLink) {
+          activeLink.classList.add('active');
+      }
+  }
+
+  navLinks.forEach(link => {
+      link.addEventListener('click', function() {
+          navLinks.forEach(nav => nav.classList.remove('active'));
+          this.classList.add('active');
+          localStorage.setItem('activeLinkId', this.id);
+      });
+  });
+});
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    if (all) {
+      select(el, all).forEach(e => e.addEventListener(type, listener))
+    } else {
+      select(el, all).addEventListener(type, listener)
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Sidebar toggle
+   */
+  if (select('.toggle-sidebar-btn')) {
+    on('click', '.toggle-sidebar-btn', function(e) {
+      select('body').classList.toggle('toggle-sidebar')
+    })
+  }
+
+  /**
+   * Search bar toggle
+   */
+  if (select('.search-bar-toggle')) {
+    on('click', '.search-bar-toggle', function(e) {
+      select('.search-bar').classList.toggle('search-bar-show')
+    })
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+})();
+// eye label js
+
+// Password Toggle eye label
+document.addEventListener('DOMContentLoaded', function () {
+  const togglePasswordButtons = document.querySelectorAll('.toggle-password');
+
+  togglePasswordButtons.forEach(function (togglePassword) {
+    togglePassword.addEventListener('click', function () {
+      const passwordField = this.previousElementSibling;
+      const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+      passwordField.setAttribute('type', type);
+      this.classList.toggle('fa-eye');
+      this.classList.toggle('fa-eye-slash');
+    });
+  });
+});
+// Upload img
+document.addEventListener('DOMContentLoaded', () => {
+  const profileImg = document.getElementById('profileImg');
+  const fileInput = document.getElementById('fileInput');
+  const overlay = document.querySelector('.overlay');
+
+  overlay.addEventListener('click', () => {
+    fileInput.click();
+  });
+
+  fileInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        profileImg.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+});
